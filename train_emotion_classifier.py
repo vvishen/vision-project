6b628588467ca6a22ffd9128403568c0f2047bdc
viksingh <viksingh@local>
2018-07-27 00:13:44 +0000

    My changes

--- train_emotion_classifier.py
+++ train_emotion_classifier.py
@@ -1,6 +1,7 @@
-from keras.layers import Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Dense, Flatten
 from keras.models import Sequential
 from keras.callbacks import Callback
+from keras.initializers import TruncatedNormal
 import pandas as pd
 import numpy as np
 import cv2
@@ -53,10 +54,65 @@ train_faces /= 255.
 val_faces /= 255.
 
 model = Sequential()
-model.add(Flatten(input_shape=input_shape))
-model.add(Dense(num_classes, activation="softmax"))
 
-model.compile(optimizer='adam', loss='categorical_crossentropy',
+#model.add(Conv2D(64,
+#    (3, 3),
+#    input_shape=(48, 48,1),
+#    activation='relu'))
+
+#model.add(Conv2D(64,
+#    (3, 3),strides=(2, 2), padding="SAME",
+#    input_shape=(48, 48,1),
+#    activation='relu', use_bias=True, kernel_initializer=TruncatedNormal(stddev=0.001)))
+
+#model.add(Dropout(0.5, input_shape=(48, 48,1)))
+
+#model.add(Conv2D(32, (5,5), padding="same", activation='relu', input_shape=(width, height, channels) ))
+
+
+model.add(Conv2D(32,
+    (3, 3), 
+    padding="same",
+    input_shape=(48, 48,1),
+    activation='relu'))
+
+model.add(Dropout(0.5))
+#model.add(Conv2D(32,
+ #   (3, 3),
+ #   input_shape=(48, 48,1),
+ #   activation='relu'))
+model.add(Conv2D(32, (3,3), padding="same", activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+#model.add(Conv2D(32,
+#    (3, 3),
+#    input_shape=(48, 48,1),
+#    activation='relu'))
+
+#model.add(Conv2D(32,
+#    (3, 3),
+#    input_shape=(48, 48,1),
+#    activation='relu'))
+
+model.add(Conv2D(32, (3,3), padding="same", activation='relu'))
+model.add(Dropout(0.5))
+model.add(Conv2D(32, (3,3), padding="same", activation='relu'))
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Dropout(0.5))
+#model.add(Conv2D(32, 4, 4,input_shape=(48, 48,1), activation='relu'))
+
+model.add(Flatten(input_shape=input_shape))
+#model.add(Dense(num_classes, activation="softmax"))
+
+model.add(Dense(num_classes, activation='relu'))
+#model.add(Dense(units = 128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='sigmoid'))
+#model.add(Dense(units = 1, activation='sigmoid'))
+
+model.compile(optimizer='adam', loss='binary_crossentropy',
 metrics=['accuracy'])
 
 model.fit(train_faces, train_emotions, batch_size=config.batch_size,
